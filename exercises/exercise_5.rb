148801a852c69c71bb9d37bf55c1585af729c936
Lisa Wild <lisakwild@gmail.com>
2024-06-27 21:03:04 -0700

    calculate total revenue, average revenue, and how many stores make over 1 mil

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts total_revenue = Store.sum(:annual_revenue)
+
+puts average_revenue = Store.average(:annual_revenue)
+
+puts one_mil_stores = Store.where("annual_revenue > 1000000").count
+
+
+# REMEMBER TO COMMIT
